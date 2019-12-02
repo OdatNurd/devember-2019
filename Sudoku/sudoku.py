7f14b03dddba2f69f0d6d0ac70661ad8b447e277
Terence Martin <github@nurdz.com>
2019-12-01 21:02:18 -0800

    Set up the initial framework for the game
    This sets up the basic framework we need in order to start on the actual
    logic of doing something with the game.
    
    The idea that we're going with here is that the game will be contained
    in its own window, which we control by hiding all of the chrome and
    blocking some key commands.
    
    The result is a plain window that the user can't generally interact with
    (for the most part) except for key bindings that we will eventually
    create.

---
+++ Sudoku/sudoku.py
@@ -0,0 +1,90 @@
+import sublime
+import sublime_plugin
+
+
+###----------------------------------------------------------------------------
+
+
+def _sudoku_syntax(file):
+    """
+    Return the full name of the given sudoku syntax based on the base name.
+    """
+    return "Packages/Sudoku/resources/syntax/%s.sublime-syntax" % file
+
+
+def _is_sudoku(obj):
+    """
+    Given a window, view or settings object, return back an indication as to
+    whether it represents a Sudoku game or not; for a window, the test applies
+    to the active view in that window.
+    """
+    if isinstance(obj, sublime.Window):
+        obj = obj.active_view().settings()
+    elif isinstance(obj, sublime.View):
+        obj = obj.settings()
+    return obj.get("syntax", "") == _sudoku_syntax("Sudoku")
+
+
+###----------------------------------------------------------------------------
+
+
+class SudokuNewGame(sublime_plugin.ApplicationCommand):
+    """
+    Start a new Sudoku game; this spawns a new window for the game and creates
+    an appropriate view inside of it to represent the game.
+    """
+    def run(self):
+        sublime.run_command("new_window")
+        window = sublime.active_window()
+
+        # Make the window bare.
+        window.set_minimap_visible(False)
+        window.set_tabs_visible(False)
+        window.set_menu_visible(False)
+        window.set_status_bar_visible(False)
+
+        # Set up the Sudoku view.
+        view = window.new_file(syntax=_sudoku_syntax("Sudoku"))
+        view.set_name("Sublime Sudoku")
+        view.set_scratch(True)
+        view.set_read_only(True)
+        view.sel().clear()
+
+
+###----------------------------------------------------------------------------
+
+
+class SudokuViewListener(sublime_plugin.ViewEventListener):
+    """
+    Listen for events inside of Sudoku views; we use these for specific
+    handling such as key bindings as well as to block some commands that ruin
+    the experience of the game.
+    """
+    @classmethod
+    def is_applicable(cls, settings):
+        return _is_sudoku(settings)
+
+    def on_text_command(self, command, args):
+        if command in ("context_menu", "drag_select", "show_scope_name"):
+            return ('noop')
+
+    def on_query_context(self, key, operator, operand, match_all):
+        if key == "sudoku":
+            return True == operand if operator == sublime.OP_EQUAL else False
+
+
+###----------------------------------------------------------------------------
+
+
+class SudokuGameListener(sublime_plugin.EventListener):
+    """
+    Listen for global Sudoku events; this covers any events that a View
+    listener can't listen for, such as window commands.
+    """
+    def on_window_command(self, window, command, args):
+        if _is_sudoku(window):
+            if command in ("new_file"):
+                return ('noop')
+
+
+###----------------------------------------------------------------------------
