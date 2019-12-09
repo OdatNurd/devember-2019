8120d8d4d0b1078a1143e1af818411a27875d941
Terence Martin <github@nurdz.com>
2019-12-08 23:12:20 -0800

    Include an initial plugin for working with gzip
    This includes the initial version of code for working with a gzipped
    file by uncompressing it and letting you work on the uncompressed
    version, recompressing every time you save.

---
+++ GZipper/gzipper.py
@@ -0,0 +1,98 @@
+import sublime
+import sublime_plugin
+
+import os
+import gzip
+import shutil
+
+
+###----------------------------------------------------------------------------
+
+
+class ReopenAsGzipCommand(sublime_plugin.TextCommand):
+    """
+    For a view that is a gzipped file, this will open up a new view that is
+    the uncompressed version of the file, then close this view. The new view
+    will re-create this gzipped file on save.
+    """
+    def run(self, edit):
+        print('running for "%s"' % self.view.file_name())
+        # Save the current file name so that we can recreate this file later.
+        org_name = self.view.file_name()
+
+        # From the name of the file, get the path and underlying file name; to
+        # do that here we just throw the .gz off the end of the filename.
+        path, file = os.path.split(org_name)
+        name, ext = os.path.splitext(os.path.splitext(file)[0])
+
+        # Create a new filename to store the uncompressed output.
+        #
+        # TODO: This will clobber files; maybe put the file in TMP or such?
+        new_name = os.path.join(path, name + "_ungz" + ext)
+
+        # Uncompress the file into the buffer now.
+        with gzip.open(org_name, 'rb') as infile:
+            with open(new_name, 'wb') as outfile:
+                shutil.copyfileobj(infile, outfile)
+
+            # Open the uncompressed file and tell it what gzipped file it's
+            # tracking.
+            # Create the view that will hold our uncompressed file, then tell it
+            # that it's tracking the new file
+            gzView = self.view.window().open_file(new_name)
+            gzView.settings().set("_gzip_name", org_name)
+
+            # Close our view now
+            self.view.close()
+
+
+    def is_enabled(self):
+        """
+        Only enable the command for files with a .gz extension (so that we can
+        get the underlying name of the file) which are also gzipped files.
+        """
+        v = self.view
+        return (v.encoding() == "Hexadecimal" and
+                v.substr(sublime.Region(0, 4)) == "1f8b" and
+                v.file_name() is not None and
+                v.file_name().endswith(".gz"))
+
+
+###----------------------------------------------------------------------------
+
+
+class GzipFileListener(sublime_plugin.ViewEventListener):
+    """
+    Event listener exclusively for Gzipped file instances; every time a file
+    is saved, the compressed version is recreated and the file is removed on
+    close.
+    """
+    @classmethod
+    def is_applicable(cls, settings):
+        return settings.has("_gzip_name")
+
+    def on_close(self):
+        os.remove(self.view.file_name())
+
+    def on_post_save(self):
+        org_filename = self.view.settings().get("_gzip_name")
+
+        with gzip.open(org_filename, 'wb') as outfile:
+            with open(self.view.file_name(), 'rb') as infile:
+                shutil.copyfileobj(infile, outfile)
+
+
+###----------------------------------------------------------------------------
+
+
+class GzipLoadListener(sublime_plugin.EventListener):
+    """
+    For every file that is loaded, attempt to re-open it as a gzip file. This
+    will only actualy trigger for files that are legitimately gzipped files;
+    the command will not enable itself for other files.
+    """
+    def on_load(self, view):
+        view.run_command("reopen_as_gzip")
+
+
+###----------------------------------------------------------------------------
