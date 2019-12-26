6b3af22551e7d1eb15c39c90dd544c7dce6f74e7
Terence Martin <github@nurdz.com>
2019-12-26 00:02:34 -0800

    Rewrite our command using our new API
    This commit (which is the last for Day 25 despite the time stamp on it)
    rewrites the existing list command to use our new API, which gets us off
    of the async thread but otherwise performs more or less like the
    previous version (sans error checking).
    
    This still requires some cleanup, error handling and spinners, but it
    does prove that indeed our concept works.

--- YouTuberizer/youtuberizer.py
+++ YouTuberizer/youtuberizer.py
@@ -4,7 +4,7 @@ import sublime_plugin
 import os
 
 
-from .networking import NetworkManager, stored_credentials_path, log
+from .networking import NetworkManager, Request, stored_credentials_path, log
 
 
 ###----------------------------------------------------------------------------
@@ -75,85 +75,22 @@ class YoutuberizerListVideosCommand(sublime_plugin.ApplicationCommand):
     in the currently active window. This will use cached credentials if there
     are any, and ask the user to log in if not.
     """
-    # The cached object for talking to YouTube; when this is None, the command
-    # will load credentials (and possibly prompt the user to log into YouTube)
-    # before continuing.
-    youtube = None
-
     def run(self):
-        # This operation might need to block, so use the async thread to run
-        # it, which is awful and terrible in many ways, but good enough until
-        # we know enough about this whole process to be able to decide on an
-        # appropriate structure.
-        sublime.set_timeout_async(lambda: self.dirty_hack())
+        netManager.request(Request("authorize"), self.result)
 
-    def dirty_hack(self):
-        print("Running the dirty hack")
-        if self.youtube == None:
-            self.youtube = get_authenticated_service()
-
-        try:
-            uploads_playlist_id = self.get_uploads_playlist()
-            if uploads_playlist_id:
-                self.get_playlist_contents(uploads_playlist_id)
-            else:
-                sublime.message_dialog("There are no uploaded videos to display")
-        except HttpError as e:
-            sublime.error_message("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
-
-
-    def get_uploads_playlist(self):
-        """
-        Retreive the playlist ID for the uploaded videos of the user that is
-        currently logged in
-
-        This can return None if there are no videos.
-        """
-
-        channels_response = self.youtube.channels().list(
-            mine=True,
-            part='contentDetails'
-        ).execute()
-
-        # From the API response, extract the playlist ID that identifies the
-        # list of videos uploaded to the authenticated user's channel.
-        for channel in channels_response['items']:
-            return channel['contentDetails']['relatedPlaylists']['uploads']
-
-        return None
-
-
-    def get_playlist_contents(self, playlist_id):
-        """
-        Given the ID of a playlsit for a user, fetch the contents of that
-        playlist.
-        """
-        playlistitems_list_request = self.youtube.playlistItems().list(
-            playlistId=playlist_id,
-            part='snippet',
-            maxResults=20
-        )
-
-        results = []
-        while playlistitems_list_request:
-            playlistitems_list_response = playlistitems_list_request.execute()
-
-            # Print information about each video.
-            for playlist_item in playlistitems_list_response['items']:
-                title = playlist_item['snippet']['title']
-                video_id = playlist_item['snippet']['resourceId']['videoId']
-                results.append([title, 'https://youtu.be/%s' % video_id])
-
-            playlistitems_list_request = self.youtube.playlistItems().list_next(
-                playlistitems_list_request, playlistitems_list_response)
-
-        results = list(sorted(results))
-        window = sublime.active_window()
-        window.show_quick_panel(results, lambda idx: self.select_video(results[idx]))
+    def result(self, request, success, result):
+        if success:
+            if request.name == "authorize":
+                netManager.request(Request("uploads_playlist"), self.result)
+            elif request.name == "uploads_playlist":
+                netManager.request(Request("playlist_contents", playlist_id=result), self.result)
+            elif request.name == "playlist_contents":
+                window = sublime.active_window()
+                window.show_quick_panel(result, lambda idx: self.select_video(result[idx]))
 
     def select_video(self, video):
         sublime.set_clipboard(video[1])
-        sublime.status_message('URL Copied: %s' % video[0] )
+        sublime.status_message('URL Copied: %s' % video[0])
 
 
 ###----------------------------------------------------------------------------
